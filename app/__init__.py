30924635caecaf02d0787b8e6181b4ed508e856d
Marat <soaptrader@gmail.com>
2022-07-21 10:51:06 +0400

    add templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
 
+#ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
 app = Flask(__name__)
+
+#def allowed_file(filename):
+#    return '.' in filename and \
+#           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
 from app import app
-
+from flask import render_template, request, redirect, url_for
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+#@app.route('/upload', methods=['POST'])
+#def upload_file():
+#    file = request.files['file']
+#    if file and allowed_file(file.filename):
+#        filename = secure_filename(file.filename)
+#        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#        return redirect(url_for('uploaded_file',
+#                                filename=filename))
+#    return redirect('/')
