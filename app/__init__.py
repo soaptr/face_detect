4d1f2773f466fe68714ae0c30fc8c8a836383b09
Marat <soaptrader@gmail.com>
2022-07-22 12:33:22 +0400

    add file upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from werkzeug.utils import secure_filename
+import os
 
-#ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
 
-#def allowed_file(filename):
-#    return '.' in filename and \
-#           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+app.config['UPLOAD_FOLDER'] = 'static/images'
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,29 @@
 from app import app
 from flask import render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+app.config['UPLOAD_FOLDER'] = 'app/static/images/'
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return render_template('index.html')
+    image = request.args.get('image')
+    if not image:
+        image='images/example.bmp'
+    return render_template('index.html', image=image)
 
-#@app.route('/upload', methods=['POST'])
-#def upload_file():
-#    file = request.files['file']
-#    if file and allowed_file(file.filename):
-#        filename = secure_filename(file.filename)
-#        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#        return redirect(url_for('uploaded_file',
-#                                filename=filename))
-#    return redirect('/')
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(app.config['UPLOAD_FOLDER'] + filename)
+        return redirect(url_for('index', image='images/'+filename))
+        #return render_template('index.html', image='images/'+filename)
+    return redirect(url_for('index'))
